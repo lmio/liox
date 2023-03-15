4088d89e696ee710565b8aa611525ccb12d2b79c
Joris Pevcevičius <joris@pevcas.eu>
2023-03-15 22:55:29 +0200

    Script for vscode extension installing & config file

---
+++ config_overlay_contest/includes.chroot/usr/share/liox-config/install_vscode_ext.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <user>"
+	exit
+fi
+
+if [ ! -d "/home/$1" ]; then
+	echo "User $1 does not exist"
+else
+	VSCODE_DIR=/home/$1/.vscode/extensions
+	su - $1 -c "mkdir -p $VSCODE_DIR"
+	if [ ! -f "$VSCODE_DIR/extensions.json" ]; then
+		echo "Creating extensions.json"
+		echo "[]" > "$VSCODE_DIR/extensions.json"
+		chown $1:$1 "$VSCODE_DIR/extensions.json"
+	fi
+	su - $1 -c "code --extensions-dir $VSCODE_DIR --install-extension ms-vscode.cpptools-extension-pack"
+fi
